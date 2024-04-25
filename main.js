29a807aa6ddee0263251b54fc3e405646bbf81f6
demianIsNotAvailable <zansky@gmail.com>
2024-04-25 10:30:17 +0200

    feat: added minimal js interaction with on/off button + channel buttons that display selected channel number

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 let isTheTvOn = false
 let currentChannel = 0
-let isMenuOpen = false
+const tvScreen = document.querySelector('.tvScreen')
+const tvOnButton = document.getElementById('on')
+const tvChannelButtons = Array.from(document.getElementsByClassName('channel'))
+
+const turnTvOn = (currentState) => {
+    currentState 
+    ? tvScreen.classList.add('tvOff')
+    : tvScreen.classList.remove('tvOff')
+    tvScreen.innerHTML = ''
+    isTheTvOn = !currentState
+}
+
+const setChannel = (input) => {
+    if (isTheTvOn) {
+        currentChannel = input
+        tvScreen.innerHTML = `<h1>${currentChannel}</h1>`
+    }
+}
+
+tvOnButton.addEventListener('click', () => turnTvOn(isTheTvOn))
+
+tvChannelButtons.forEach((button) => {
+    button.addEventListener('click', () => setChannel(button.innerHTML))
+})
